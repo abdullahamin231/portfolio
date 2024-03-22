b27d654bc34a07502f08e2f5efafbad0cbc3ef49
Abdullah Amin <abdullahamin231@gmail.com>
2024-03-22 22:44:38 +0500

    done

--- src/assets/right.jsx
+++ src/assets/right.jsx
@@ -2,32 +2,46 @@ import React from 'react';
 import mail from "./mail.png";
 import blog from "./blog.png";
 import poke from "./poke.gif";
+import shop from "./shop.gif";
 import chat from "./chat.png";
-import "../App.css"
+import chess from "./chess.png";
+import weather from "./weather.jpeg";
+import sort from "./sort.webm";
+
+import "../App.css";
 
 const all = [
     {i: blog, title: "Blogging Website", link: "https://www.youtube.com/watch?v=CvYHiHCe7gI&feature=youtu.be"},
     {i: poke, title: "Memory Card Game", link: "https://www.github.com/abdullahamin/memory-card"},
     {i: chat, title: "Chat Application", link: "https://www.youtube.com/watch?v=i0BmRc4yxlM"},
+    {i: shop, title: "E-Commerce Website", link: "https://github.com/abdullahamin231/shopping-website"},
+    {i: chess, title: "Chess Game", link: "https://github.com/abdullahamin231/chess"},
+    {i: weather, title: "Weather App", link: "https://github.com/abdullahamin231/weather-app"},
+    {i: sort, title: "Sorting Visualizer", link: "https://github.com/abdullahamin231/sorting-algorithm-visualizer"},
 ];
 
 const Proj = ({ title, i, link }) => {
+    const isVideo = i.endsWith('.webm') || i.endsWith('.gif');
+    
     return (
-        <div className="w-[80%] mt-4 bg-[#fafafa] rounded-lg px-4 py-2 font-inter shadow-lg h-fit">
-                <a href={link} target='_blank'>
-            <div className='font-bold font-inter text-xl text-slate-800 py-2'>{title}</div>
-            <div>
-                <img src={i} alt="" />
-            </div>
-                </a>
+        <div className="w-[80%] mt-4 bg-[#deeaff] rounded-lg px-4 py-2 font-inter shadow-lg h-fit">
+            <a href={link} target='_blank' rel="noopener noreferrer">
+                <div className='font-bold font-inter text-xl text-slate-800 py-2'>{title}</div>
+                <div>
+                    {
+                        isVideo ? <video className="w-full" autoPlay loop muted src={i}></video> :
+                        <img src={i} alt="" />  
+                    }
+                </div>
+            </a>
         </div>
     );
 };
 
 const Right = () => {
     return (
-        <div className="flex flex-col  items-center">
-            <div className="w-[760px]   bg-white rounded-lg px-6 py-8 font-inter shadow-lg h-fit">
+        <div className="flex flex-col items-center">
+            <div className="w-[760px] bg-white rounded-lg px-6 py-8 font-inter shadow-lg h-fit">
                 <div className='font-bold font-inter text-xl text-slate-800'>About Me</div>
                 <div className='text-gray-600'>
                     Fullstack developer from Islamabad, Pakistan. Currently pursuing a degree in Computer Science from FAST University Islamabad.
@@ -37,9 +51,9 @@ const Right = () => {
                     <span><img className="max-w-[20px]" src={mail} alt="" /></span> abdullahamin231@gmail.com
                 </a>
             </div>
-            <div className="w-[760px]  bg-white rounded-lg mt-4 px-6 py-8 font-inter shadow-lg h-fit">
-                <div className='font-bold font-inter text-xl text-slate-800 border-b-[#0b0e0e39] border-b-[1px] pb-4'>Projects</div>
-                <div>
+            <div className="w-[760px] bg-white rounded-lg overflow-scroll mt-4 px-6 py-8 font-inter shadow-lg h-[fit-content]">
+                <div className='font-bold font-inter text-xl  text-slate-800 border-b-[#0b0e0e39] border-b-[1px] pb-4'>Projects</div>
+                <div className='flex flex-row flex-wrap items-center gap-2  overflow-x-auto'>
                     {all.map((item, index) => (
                         <Proj key={index} title={item.title} i={item.i} link={item.link} />
                     ))}
